c59fcd81d9fced0818d840267537d3c5a6586687
Ferdhika Yudira <rpl4rt08@gmail.com>
2020-10-20 14:06:09 +0700

    Updates

---
+++ precache-manifest.7ec516e2ed738af745d3afbcc0edbfa0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96bcabfdc6e79701c7283f1ec020ec51",
+    "url": "/curl-builder/index.html"
+  },
+  {
+    "revision": "edc82c9a6087950acd18",
+    "url": "/curl-builder/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "8010fae6d62e6b92a2e9",
+    "url": "/curl-builder/static/css/main.c94c5519.chunk.css"
+  },
+  {
+    "revision": "edc82c9a6087950acd18",
+    "url": "/curl-builder/static/js/2.36220365.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/curl-builder/static/js/2.36220365.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8010fae6d62e6b92a2e9",
+    "url": "/curl-builder/static/js/main.20d31c3d.chunk.js"
+  },
+  {
+    "revision": "7c36a1dd00c3a03a6e88",
+    "url": "/curl-builder/static/js/runtime-main.2afd85e1.js"
+  }
+]);
